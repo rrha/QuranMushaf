df3645b0821d4eb172516166c9e413026bc0ed6b
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-11-17 14:57:16 +0100

    fixes

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,30 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace src
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int counter = 1;
+        string addOnString = "00";
         public MainWindow()
         {
             InitializeComponent();
-            PagesWindow ayat = new PagesWindow();
-            ayat.Show();
+        }
+
+        private void CheckCounter()
+        {
+            if (counter >= 1 && counter <= 9)
+            {
+                addOnString = "00";
+            }
+            else if (counter >= 10 && counter <= 99)
+            {
+                addOnString = "0";
+            }
+            else if (counter >= 100)
+            {
+                addOnString = "";
+            }
+        }
+        private void ForwardBtn(object sender, RoutedEventArgs e)
+        {
+            CheckCounter();
+            if (counter < 604)
+            {
+                counter += 1;
+            }
+            PageImage.Source = new BitmapImage(new Uri(@"pages/" + "page" + addOnString + counter.ToString() + ".png", UriKind.Relative));
+        }
+
+        private void BackwardBtn(object sender, RoutedEventArgs e)
+        {
+            CheckCounter();
+            if (counter > 1)
+            {
+                counter -= 1;
+            }
+            PageImage.Source = new BitmapImage(new Uri(@"pages/" + "page" + addOnString + counter.ToString() + ".png", UriKind.Relative));
         }
     }
 }
